dc60b40ec0c7914e7d946cedd40d13839d2b4f36
Jadakis <44173632+Jadakis@users.noreply.github.com>
2018-12-21 09:37:30 -0500

    Add files via upload

---
+++ Lesson 10/problem1.py
@@ -0,0 +1,11 @@
+print('-'*65)
+print('Life Decision Bot:')
+print()
+snowing = input('Is it snowing outside? ')
+if snowing == 'No':
+	print('Stay at home! ')
+	print('Maybe play a bit of Mortal Kombat Xl? ')
+else:
+	print('Go out and make a snow angel! ')
+	print('Maybe make a snowman! ')
+print('-'*65)
---
+++ Lesson 10/problem3.py
@@ -0,0 +1,10 @@
+print('-'*65)
+print('Welcome to Club45, the most lit club in downtown ACTvF. Before you can enter, I need you to answer some questions. . .')
+print()
+age = input('What is your age today? ')
+age = int(age)
+if age >= 21:	
+	print('Cool, come in. ')
+else:
+	print('Your gonna need to back up. This club is 21+ only so find somewhere else to party or find out what robot punches feel like.')
+	print('-'*65) 
